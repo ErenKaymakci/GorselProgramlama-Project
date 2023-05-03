b44eee91a6955b475f09769adc72d3e463a7cfe2
Zekeriya AY <zekeriya@zekeriyaay.com>
2023-05-03 11:48:36 +0300

    Revert "Merge branch 'main' into devZ"
    This reverts commit 91226249a182e8a12d0f2d426375cac461ca3642, reversing
    changes made to c8d82c57622367a46ca7a6aece995754cbf094ef.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,17 +29,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonMenu = new System.Windows.Forms.Button();
+            this.buttonAccountDetails = new System.Windows.Forms.Button();
+            this.userControlAccountDetails = new GelismisATM.UserControlAccountDetails();
+            this.userControlMenu = new GelismisATM.UserControlMenu();
             this.SuspendLayout();
+            // 
+            // buttonMenu
+            // 
+            this.buttonMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(137)))), ((int)(((byte)(6)))));
+            this.buttonMenu.FlatAppearance.BorderSize = 0;
+            this.buttonMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMenu.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(254)))));
+            this.buttonMenu.Location = new System.Drawing.Point(76, 175);
+            this.buttonMenu.Name = "buttonMenu";
+            this.buttonMenu.Size = new System.Drawing.Size(300, 65);
+            this.buttonMenu.TabIndex = 16;
+            this.buttonMenu.Text = "MENÜ";
+            this.buttonMenu.UseVisualStyleBackColor = false;
+            this.buttonMenu.Click += new System.EventHandler(this.buttonMenu_Click);
+            // 
+            // buttonAccountDetails
+            // 
+            this.buttonAccountDetails.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(137)))), ((int)(((byte)(6)))));
+            this.buttonAccountDetails.FlatAppearance.BorderSize = 0;
+            this.buttonAccountDetails.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAccountDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAccountDetails.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(254)))));
+            this.buttonAccountDetails.Location = new System.Drawing.Point(76, 295);
+            this.buttonAccountDetails.Name = "buttonAccountDetails";
+            this.buttonAccountDetails.Size = new System.Drawing.Size(300, 65);
+            this.buttonAccountDetails.TabIndex = 17;
+            this.buttonAccountDetails.Text = "HESAP DETAYLARI";
+            this.buttonAccountDetails.UseVisualStyleBackColor = false;
+            this.buttonAccountDetails.Click += new System.EventHandler(this.buttonAccountDetails_Click);
+            // 
+            // userControlAccountDetails
+            // 
+            this.userControlAccountDetails.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(14)))), ((int)(((byte)(23)))));
+            this.userControlAccountDetails.Location = new System.Drawing.Point(-5, -30);
+            this.userControlAccountDetails.Name = "userControlAccountDetails";
+            this.userControlAccountDetails.Size = new System.Drawing.Size(1024, 768);
+            this.userControlAccountDetails.TabIndex = 19;
+            // 
+            // userControlMenu
+            // 
+            this.userControlMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(14)))), ((int)(((byte)(23)))));
+            this.userControlMenu.Location = new System.Drawing.Point(-5, -30);
+            this.userControlMenu.Name = "userControlMenu";
+            this.userControlMenu.Size = new System.Drawing.Size(1024, 768);
+            this.userControlMenu.TabIndex = 18;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(14)))), ((int)(((byte)(23)))));
             this.ClientSize = new System.Drawing.Size(1008, 729);
-            this.Location = new System.Drawing.Point(15, 15);
+            this.Controls.Add(this.userControlAccountDetails);
+            this.Controls.Add(this.userControlMenu);
+            this.Controls.Add(this.buttonAccountDetails);
+            this.Controls.Add(this.buttonMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
+            this.Text = "Form1";
             this.ResumeLayout(false);
+
         }
 
         #endregion
